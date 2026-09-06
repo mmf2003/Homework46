73d2e4a5326d53e351f4ac10e6b31065790deedf
Sidelnikov Oleksandr <mmf2003@ukr.netr>
2026-09-06 15:25:39 +0300

    Added 10 users

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import {
@@ -9,19 +10,44 @@ import {
 import { fetchUser } from "../redux/asyncActions/fetchUser";
 
 function UserProfile() {
+    const [selectedUserId, setSelectedUserId] = useState(1);
+
     const user = useSelector(selectUser);
     const loading = useSelector(selectLoading);
     const error = useSelector(selectError);
 
     const dispatch = useDispatch();
 
+    const userIds = Array.from({ length: 10 }, (_, index) => index + 1);
+
     const handleLoadUser = () => {
-        dispatch(fetchUser(1));
+        dispatch(fetchUser(selectedUserId));
     };
 
     return (
         <div className="user-profile">
             <h3>User Profile</h3>
+
+            <div className="user-picker">
+                <p className="user-picker__label">Select User:</p>
+
+                <div className="user-picker__buttons">
+                    {userIds.map((id) => (
+                        <button
+                            key={id}
+                            type="button"
+                            className={
+                                selectedUserId === id
+                                    ? "user-picker__button active"
+                                    : "user-picker__button"
+                            }
+                            onClick={() => setSelectedUserId(id)}
+                        >
+                            {id}
+                        </button>
+                    ))}
+                </div>
+            </div>
 
             {loading && <p className="loading-message">Loading user...</p>}
 
